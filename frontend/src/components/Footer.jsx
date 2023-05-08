a4fe6e9b247b57538eea77ebfcd71c3c8012afaf
duclm278 <palered548@gmail.com>
2023-05-08 19:03:16 +0700

    Init login, profile & learning pages

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import { Link, Typography } from "@mui/material";
+
+export default function Footer() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="#">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
---
+++ frontend/src/pages/learn/LearningList.jsx
@@ -0,0 +1,22 @@
+export default function LearningList() {
+  return (
+    <div>
+      <ul>
+        <li>Introduction</li>
+        <li>What is React?</li>
+        <li>Why React?</li>
+        <li>React vs Angular vs Vue</li>
+        <li>React vs React Native</li>
+        <li>React vs Next.js</li>
+        <li>React vs Gatsby</li>
+        <li>React vs jQuery</li>
+        <li>React vs Angular</li>
+        <li>React vs Vue</li>
+        <li>React vs Svelte</li>
+        <li>React vs Web Components</li>
+        <li>React vs Flutter</li>
+        <li>React vs Ionic</li>
+      </ul>
+    </div>
+  );
+}
