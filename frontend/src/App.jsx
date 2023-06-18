04bbfd77125da45e951a4655824f2653e0eac9af
duclm278 <palered548@gmail.com>
2023-06-18 23:03:03 +0700

    New theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,19 @@
-import { CssBaseline } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Router from "./routes";
+import theme from "./themes/mui";
 
 function App() {
   return (
     <LocalizationProvider dateAdapter={AdapterMoment}>
       <BrowserRouter>
-        <CssBaseline />
-        <Router />
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Router />
+        </ThemeProvider>
       </BrowserRouter>
     </LocalizationProvider>
   );
